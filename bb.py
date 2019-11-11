43d2ebc39141888119dc789e5d3cac43a9bf4ead
Femi Osinowo <femiosinowo@gmail.com>
2019-11-10 20:39:43 -0500

    Update bb.py

--- bb.py
+++ bb.py
@@ -1,3 +1,4 @@
+import airflow
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.contrib.kubernetes.secret import Secret
 from airflow.contrib.kubernetes.volume import Volume
